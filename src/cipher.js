7d060cc7e96505b7d66dc1df1b756f440fc794bd
Paula Amigo Diaz <paulaamigodiaz@MacBook-Air-de-Paula.local>
2022-07-13 12:19:44 -0400

    second

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,48 @@
 const cipher = {
-  // ...
+  encode: function (mensajeOriginal, shiftInput) {
+    let mensajeFinal = "";
+    
+    for(let i = 0; i< mensajeOriginal.length; i++) {
+     let ascii_num = mensajeOriginal[i].charCodeAt()
+     
+    
+     if(ascii_num >=65 && ascii_num <=90){
+         let sum = (ascii_num - 65 + parseInt(shiftInput))%26 + 65;
+     
+         console.log(sum)
+         mensajeFinal +=String.fromCharCode(sum)
+     }
+     else{
+         mensajeFinal += mensajeOriginal[i]
+         console.log(mensajeFinal)
+     }
+ 
+     console.log(mensajeFinal)
+ }
+ return mensajeFinal
+},
+decode: function (mensajeOriginal, shiftInput) {
+  let mensajeFinal = "";
+  for(let i = 0; i< mensajeOriginal.length; i++) {
+      let ascii_num = mensajeOriginal[i].charCodeAt()
+      
+     
+      if(ascii_num >=65 && ascii_num <=90){
+          let sum = (ascii_num - 65 - parseInt(shiftInput))%26 + 65;
+      
+          console.log(sum)
+          mensajeFinal +=String.fromCharCode(sum)
+      }
+      else{
+          mensajeFinal += mensajeOriginal[i]
+          console.log(mensajeFinal)
+      }
+  
+      console.log(mensajeFinal)
+  }
+  return mensajeFinal 
+}
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
-const alphabet= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+let mensajeFinal = document.getElementById("texto2")
+
+const botonCifrar = document.getElementById("encode")
+
+const botonDescifrar = document.getElementById("decode")
+
+botonCifrar.addEventListener("click", seleccionar)
+function seleccionar(){ 
+    let mensajeOriginal = document.getElementById("texto1").value.toUpperCase();
+    let input = document.getElementById("shiftInput").value;
+    
+    document.getElementById("texto2").value = cipher.encode(mensajeOriginal, input);
+    }
+
+botonDescifrar.addEventListener("click", presionar)
+function presionar (){
+    let mensajeOriginal = document.getElementById("texto1").value.toUpperCase();
+    let input = document.getElementById("shiftInput").value;
+
+    document.getElementById("texto2").value = cipher.decode(mensajeOriginal, input);
+}
+
+
+
+const descrifrar = document.getElementById("descrifrar")
+//Que muestre el mensaje descrifrado en el recuadro de resultado
