95dfdf6b354da79a02085551c69c1fbb07611670
Paula <paula.amigo@hotmail.cl>
2022-12-14 12:08:21 -0300

    css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import cipher from "./cipher.js";
-window.cipher = cipher
+window.cipher = cipher;
 const botonCifrar = document.getElementById("encode");
-
 const botonDescifrar = document.getElementById("decode");
+
+//popup que es
+const popup1 = document.querySelector("#popup1");
+const queEs = document.querySelector(".queEs");
+const cerrar1 = document.querySelector(".entendido");
+
+queEs.addEventListener("click", () => {
+  popup1.show();
+});
+cerrar1.addEventListener("click", () => {
+  popup1.close();
+});
+
+//popup como funciona
+const popup2 = document.querySelector("#popup2");
+const comoFunciona = document.querySelector(".comoFunciona");
+const cerrar2 = document.querySelector(".vamos");
+
+comoFunciona.addEventListener("click", () => {
+  popup2.show();
+});
+cerrar2.addEventListener("click", () => {
+  popup2.close();
+});
 
 botonCifrar.addEventListener("click", seleccionar);
 function seleccionar() {
